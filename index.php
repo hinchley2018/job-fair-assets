0f69efd7a62f84df622c35c4eaead224bdc51cad
jhinchley <dev.jjh@planet-hinchley.com>
2016-07-19 22:08:13 -0500

    Trying to simple output of booth

--- index.php
+++ index.php
@@ -12,20 +12,26 @@
   <body>
   <?php
 
-    $query = "SELECT booth_id FROM ScantronInv";
+    //we want to restrict this query so we can output
+    //the booths like the drawing
+    //look at FairMap.svg to see the layout
+    $booth_query = "SELECT booth_id,Name FROM Company";
 
     try {
-        $sth = $db->prepare($query);
+        $sth = $db->prepare($booth_query);
         $result=$sth->execute();
     }
     catch (PDOException $e) {
         // Note: On a production website, you should not output $ex->getMessage().
         // It may provide an attacker with helpful information about your code.
-        die("Failed to run query: ");//. $ex->getMessage()
+        die("Failed to run booth query: ");//. $ex->getMessage()
     }
 
     //displays all information on table
     $rows = $sth->fetchAll();
+    foreach ($rows as $row) {
+      echo '<input type="submit" name="' . $row['booth_id'] . '" value="' . $row['Name'] . '"/>';
+    }
   ?>
     <a href="http://www.w3schools.com">
       <img border="0" alt="W3Schools" src="assets/Images/FairMap.png" width="1000" height="1000">
