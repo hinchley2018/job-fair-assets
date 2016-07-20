1d8e33499abaaac4f7db4675944819c6a0f62517
jhinchley <dev.jjh@planet-hinchley.com>
2016-07-19 21:43:09 -0500

    corrected included file Added head tag

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
+<?php
+  //connect to db and start session
+  require("db_connect.php");
+?>
 <!DOCTYPE html>
 <html>
-<body>
-<a href="http://www.w3schools.com">
-  <img border="0" alt="W3Schools" src="assets/Images/FairMap.png" width="1000" height="1000">
-</a>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Career Fair Locator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <a href="http://www.w3schools.com">
+      <img border="0" alt="W3Schools" src="assets/Images/FairMap.png" width="1000" height="1000">
+    </a>
+  </body>
 </html>
