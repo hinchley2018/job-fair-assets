9343155422a19955e56c63b20e320d6db7a45205
jhinchley <dev.jjh@planet-hinchley.com>
2016-07-19 21:50:23 -0500

    added simple query to select from db

--- index.php
+++ index.php
@@ -10,6 +10,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
+  <?php
+
+    $query = "SELECT booth_id FROM ScantronInv";
+
+    try {
+        $sth = $db->prepare($query);
+        $result=$sth->execute();
+    }
+    catch (PDOException $e) {
+        // Note: On a production website, you should not output $ex->getMessage().
+        // It may provide an attacker with helpful information about your code.
+        die("Failed to run query: ");//. $ex->getMessage()
+    }
+
+    //displays all information on table
+    $rows = $sth->fetchAll();
+  ?>
     <a href="http://www.w3schools.com">
       <img border="0" alt="W3Schools" src="assets/Images/FairMap.png" width="1000" height="1000">
     </a>
