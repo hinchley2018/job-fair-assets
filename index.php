a84ac1ec5a735c417c2847c0bdea425ae64aa6b3
jhinchley <dev.jjh@planet-hinchley.com>
2016-07-20 06:29:06 -0500

    moved styling to stylesheet included it in head tag added styling code to check if div class contains specfied character

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Career Fair Locator</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
@@ -33,11 +34,9 @@
     //displays all information on table
     $rows = $sth->fetchAll();
     foreach ($rows as $row) {
-      echo '<input type="submit" name="' . $row['booth_id'] . '" value="' . $row['booth_id'] . '"/>';
+      echo '<div><input type="submit" name="' . $row['booth_id'] . '" value="' . $row['booth_id'] . '"/></div>';
     }
   ?>
-    <a href="http://www.w3schools.com">
-      <img border="0" alt="W3Schools" src="assets/Images/FairMap.png" width="1000" height="1000">
-    </a>
+    <img border="0" alt="Map" src="assets/Images/FairMap.png" width="1000" height="1000">
   </body>
 </html>
