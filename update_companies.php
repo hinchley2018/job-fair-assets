ade9daa26ef5eeb5e4f029778ce7070e7a2dd49e
jhinchley <dev.jjh@planet-hinchley.com>
2016-07-21 21:43:32 -0500

    syntax error

--- update_companies.php
+++ update_companies.php
@@ -13,35 +13,34 @@
     $advanced_query = "INSERT INTO Company_Data(booth_id,url) VALUES(:booth_id,:url)";
 
     $booths = array("A","B","C","D","E","F","G","H","I","J","K","L");
-    for ($tens_place=0; $tens_place<=1; $tens_place++){
+    for ($tens_place=0; $tens_place<=1; $tens_place++) {
 
-        for ($ones_place=1; $ones_place<=9; $ones_place++){
-            foreach ($booths as $booth)  {
+        for ($ones_place = 1; $ones_place <= 9; $ones_place++) {
+            foreach ($booths as $booth) {
 
                 try {
                     $sth = $db->prepare($booth_query);
                     $query_params = array(
-                        ':booth_id' => $booth.$tens_place.$ones_place,
-                        ':Name'=>"Company".$booth.$tens_place.$ones_place
+                        ':booth_id' => $booth . $tens_place . $ones_place,
+                        ':Name' => "Company" . $booth . $tens_place . $ones_place
                     );
-                    $result=$sth->execute($query_params);
+                    $result = $sth->execute($query_params);
 
                     $sth2 = $db->prepare($advanced_query);
                     $query_params2 = array(
-                        ':booth_id' => $booth.$tens_place.$ones_place,
-                        ':url'=>"www.google.com"
+                        ':booth_id' => $booth . $tens_place . $ones_place,
+                        ':url' => "www.google.com"
                     );
-                    $result=$sth2->execute($query_params2);
-                }
-                catch (PDOException $e) {
+                    $result = $sth2->execute($query_params2);
+                } catch (PDOException $e) {
                     // Note: On a production website, you should not output $ex->getMessage().
                     // It may provide an attacker with helpful information about your code.
                     die("Failed to run booth query: ");//. $e->getMessage()
                 }
+            }
+
         }
-
     }
-
 
 
 ?>
