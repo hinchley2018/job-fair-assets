733d7b7c5ee5b6032a453584fdd03b428fc33753
jhinchley <dev.jjh@planet-hinchley.com>
2016-08-14 12:17:38 -0500

    special formatting fix

--- index.php
+++ index.php
@@ -20,8 +20,7 @@
     //output by section
     $booth_query = "SELECT Company.booth_id,Company.Name,Company_Data.url FROM Company LEFT JOIN Company_Data ON Company.booth_id=Company_Data.booth_id WHERE Company.booth_id BETWEEN :start_booth_id AND :end_booth_id";
 
-    $booths = array(array("A01","A03"),array("A04","A07"),array("A08","A11"),array("A12","A14"),
-        array("B01","B17"),
+    $booths = array(array("B01","B17"),
         array("C01","C17"),
         array("D01","D17"),
         array("E01","E17"),
@@ -51,40 +50,82 @@
         array("113","115"),
         array("116","118")*/
     );
-    echo '<form method="post" action="AdvancedDescription.php">>';
-    foreach ($booths as $booth)  {
+    echo '<form method="post" action="AdvancedDescription.php">';
 
-    try {
+    //do special formatting here
+    echo '<div class="A">';
+    $special_booths = array(array("A01","A03"),
+        array("A04","A07"),
+        array("A08","A11"),
+        array("A12","A14"));
+    foreach ($special_booths as $special_booth){
+      try{
         $sth = $db->prepare($booth_query);
-        $query_params = array(':start_booth_id' => $booth[0],
-                ':end_booth_id' => $booth[1]);
+        $query_params = array(':start_booth_id'=>$special_booth[0],
+          'end_booth_id'=>$special_booth[1]);
         $result=$sth->execute($query_params);
-    }
-    catch (PDOException $e) {
+      }
+      catch (PDOException $e){
         // Note: On a production website, you should not output $ex->getMessage().
         // It may provide an attacker with helpful information about your code.
-        die("Failed to run booth query: ");//. $e->getMessage()
-    }
+        die("Failed to run booth query: ");//. $e->getMessage()}
+      }
+      $rows = $sth->fetchAll();
+      echo '<div class="'. $special_booth[0] .'">';
+      foreach ($rows as $row) {
+        $content = " <div>
+          <a href='http://" . $row['url'] ."'><b>Career Site</b></a>
+          <input type='submit' name='View" . $row['booth_id'] . "' value='View'/>
+          <br>
+          <input type='submit' name='Save" . $row['booth_id'] . "' value='Save'/>
+        </div>";
+        echo '<a href="#" data-html="true"
+           data-toggle="popover"
+           title="<b>' . $row['Name'] .'</b>"
+           data-content="'.$content.'">'.$row['booth_id'].'
+        </a>';
+      }
+      echo '    </div>';
 
-    //displays all information on table
-    $rows = $sth->fetchAll();
-    //section the booths by angles
-    echo '<div class="'. $booth[0] .'">';
-    foreach ($rows as $row) {
-      $content = " <div>
-        <a href='http://" . $row['url'] ."'><b>Career Site</b></a>
-        <input type='submit' name='View" . $row['booth_id'] . "' value='View'/>
-        <br>
-        <input type='submit' name='Save" . $row['booth_id'] . "' value='Save'/>
-      </div>";
-      echo '<a href="#" data-html="true"
-         data-toggle="popover"
-         title="<b>' . $row['Name'] .'</b>"
-         data-content="'.$content.'">'.$row['booth_id'].'
-      </a>';
     }
     echo '</div>';
+
+
+    //normal formatting
+    foreach ($booths as $booth)  {
+
+      try {
+          $sth = $db->prepare($booth_query);
+          $query_params = array(':start_booth_id' => $booth[0],
+                  ':end_booth_id' => $booth[1]);
+          $result=$sth->execute($query_params);
+      }
+      catch (PDOException $e) {
+          // Note: On a production website, you should not output $ex->getMessage().
+          // It may provide an attacker with helpful information about your code.
+          die("Failed to run booth query: ");//. $e->getMessage()
+      }
+
+      //displays all information on table
+      $rows = $sth->fetchAll();
+      //section the booths by angles
+      echo '<div class="'. $booth[0] .'">';
+      foreach ($rows as $row) {
+        $content = " <div>
+          <a href='http://" . $row['url'] ."'><b>Career Site</b></a>
+          <input type='submit' name='View" . $row['booth_id'] . "' value='View'/>
+          <br>
+          <input type='submit' name='Save" . $row['booth_id'] . "' value='Save'/>
+        </div>";
+        echo '<a href="#" data-html="true"
+           data-toggle="popover"
+           title="<b>' . $row['Name'] .'</b>"
+           data-content="'.$content.'">'.$row['booth_id'].'
+        </a>';
+      }
+      echo '</div>';
     }
+
     echo '</form>';
   ?>
 
