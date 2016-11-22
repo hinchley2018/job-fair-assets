3b633858ce92c5f5fa2a4f4106ccb9f21fe89ee1
jhinchley <dev.jjh@planet-hinchley.com>
2016-11-22 17:55:34 -0600

    row styling

--- admin.php
+++ admin.php
@@ -15,7 +15,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
-  <body background="assets/Images/FairMap.svg">
+  <body background="assets/Images/FairMap.png">
 
   <?php
 
