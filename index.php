0a56601d5da5d8d9cccb7dba6d98b982d6c17617
jhinchley <dev.jjh@planet-hinchley.com>
2016-11-22 21:45:53 -0600

    row styling

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
       <link rel="stylesheet" href="assets/css/zone.css">
-<!--      <link rel="stylesheet" href="assets/css/rows.css">-->
+      <link rel="stylesheet" href="assets/css/rows.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
