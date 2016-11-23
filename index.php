d60e960ea890736add93e4490ba8c5b2fc6b14ef
jhinchley <dev.jjh@planet-hinchley.com>
2016-11-22 21:39:23 -0600

    row styling

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
   //connect to db and start session
   require("db_connect.php");
   include("zoneFunctions.php");
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -69,7 +71,54 @@
   );
 
   echo '<form method="post" action="AdvancedDescription.php">';
+?>
+  <!--searchbar
+  Link for snippet http://bootsnipp.com/snippets/2q81r
+  Originally created by maridlcrmn
+  -->
+  <div class="container">
+      <div class="row">
+          <div class="col-md-12">
+              <div class="input-group" id="adv-search">
+                  <input type="text" class="form-control" placeholder="Search for snippets" />
+                  <div class="input-group-btn">
+                      <div class="btn-group" role="group">
+                          <div class="dropdown dropdown-lg">
+                              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
+                              <div class="dropdown-menu dropdown-menu-right" role="menu">
+                                  <form class="form-horizontal" role="form">
+                                      <div class="form-group">
+                                          <label for="filter">Filter by</label>
+                                          <select class="form-control">
+                                              <option value="0" selected>All Snippets</option>
+                                              <option value="1">Featured</option>
+                                              <option value="2">Most popular</option>
+                                              <option value="3">Top rated</option>
+                                              <option value="4">Most commented</option>
+                                          </select>
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="contain">Author</label>
+                                          <input class="form-control" type="text" />
+                                      </div>
+                                      <div class="form-group">
+                                          <label for="contain">Contains the words</label>
+                                          <input class="form-control" type="text" />
+                                      </div>
+                                      <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                                  </form>
+                              </div>
+                          </div>
+                          <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  </div>
 
+  <?php
   ouputZone($db,$booth_query,"Zone1",$zone1);
   echo "<br><br><br>";
   ouputZone($db,$booth_query,"Zone2",$zone2);
